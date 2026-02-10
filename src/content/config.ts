95b48ab10752bd9c8be939a54e7070d46753795a
Cyril kurillos <bocage.cyril@gmail.com>
2026-02-10 21:55:52 +0100

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		category: z.enum(['Actu', 'Analyse', 'Draft', 'Histoire', 'Breaking News']),
-		author: z.string(),
-		// Ajoute ces deux lignes ici :
-		tags: z.array(z.string()).default(["Miami Dolphins"]), 
-		isDraft: z.boolean().default(false),
-	}),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+        category: z.enum(['Actu', 'Analyse', 'Draft', 'Histoire', 'Breaking News']),
+        author: z.string(),
+        tags: z.array(z.string()).default(["Miami Dolphins"]),
+        isDraft: z.boolean().default(false),
+    }),
 });
 
 export const collections = { blog };
