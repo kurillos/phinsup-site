78ff989a8da0cd2d3b4769fba8b312e1af402fa2
Cyril kurillos <bocage.cyril@gmail.com>
2026-02-12 11:42:03 +0100

    Fix: espacement dans les articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+      // On injecte le plugin Typography ici
+      configFile: './tailwind.config.mjs', 
+    }),
+  ],
 });
